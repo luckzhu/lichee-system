0095419bbbb4cf793db61be0caa90faef959909e
luckzhu <luckzhu1992@gmail.com>
2020-05-14 10:27:01 +0800

    build: lb-table component

---
+++ src/components/lb-table/lb-render.vue
@@ -0,0 +1,14 @@
+/* eslint-disable */
+<script>
+export default {
+  name: 'LbRender',
+  functional: true,
+  props: {
+    scope: Object,
+    render: Function
+  },
+  render: (h, ctx) => {
+    return ctx.props.render ? ctx.props.render(h, ctx.props.scope) : ''
+  }
+}
+</script>
